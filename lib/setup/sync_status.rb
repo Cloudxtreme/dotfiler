d5beb5f750906c6eb24a3f68955d7458930c74ed
Artur Spychaj <drognanar@gmail.com>
2017-01-15 22:00:45 -0800

    WIP: SyncStatus improvements

--- lib/setup/sync_status.rb
+++ lib/setup/sync_status.rb
@@ -4,49 +4,66 @@ module Setup
 STATUS_KINDS = {
   error: 'error',
   up_to_date: 'up to date',
-  backup: 'backup',
+  backup: 'needs sync',
+  restore: 'needs sync',
+  resync: 'needs sync',
   overwrite_data: 'differs'
 }
 
-# Current synchronization status of a Task.
-class SyncStatus < Struct.new(:name, :kind, :status_msg)
-  def self.error(status_msg = nil)
-    SyncStatus.new :error, status_msg
-  end
-end
-
-# Current synchronization status of a Package.
-class GroupStatus < Struct.new(:items, :status_msg)
-  def kind
-    items.map(&:kind).uniq.length == 1 ? items[0].kind : nil
-  end
-end
+# TODO: Use a string builder to build the status_str
 
 module Status
 
-def item_str(item)
-  item_str_with_level item, 0
-end
-
-def status_str(status)
-  kind = STATUS_KINDS[status.kind]
-  status.status_msg.nil? ? kind : "#{kind}: #{status.status_msg}"
+# Returns a stirng representation of a status including subitems.
+def status_str
+  status_str_with_level 0
 end
 
 private
 
-def item_str_with_level(item, level)
-  name = item.name
-  if name.nil?
-    if item.children?
-      item.map { |subitem| item_str_with_level subitem, level }.join '\n'
-    end
-  else
-    status = item.info
-    # ''.ljust(level).
+# Returns a string representation of a status excluding subitems.
+def status_str_single
+  return "#{name}:" if kind.nil?
+  kind_str = STATUS_KINDS[kind]
+
+  status_msg.nil? ? "#{name}: #{kind_str}" : "#{name}: #{kind_str}: #{status_msg}"
+end
+
+end
+
+# Current synchronization status of a Task.
+class SyncStatus < Struct.new(:name, :kind, :status_msg)
+  include Status
+
+  def self.error(status_msg = nil)
+    SyncStatus.new :error, status_msg
+  end
+
+  private
+
+  def status_str_with_level(level)
+    status_str_single
   end
 end
 
+# Current synchronization status of a Package.
+class GroupStatus < Struct.new(:name, :items, :status_msg)
+  include Status
+
+  def kind
+    items.map(&:kind).uniq.length == 1 ? items[0].kind : nil
+  end
+
+  private
+
+  def status_str_with_level(level)
+    if name.nil?
+      items.map { |subitem| subitem.status_str_with_level level }.join '\n'
+    else
+      subitem_status_str = items.map { |subitem| subitem.status_str_with_level(level + 1) }.join '\n'
+      status_str_single
+    end
+  end
 end
 
 end
--- spec/setup/sync_status_spec.rb
+++ spec/setup/sync_status_spec.rb
@@ -9,36 +9,78 @@ RSpec.describe SyncStatus do
     expect(status.kind).to eq(:up_to_date)
     expect(status.status_msg).to eq('message')
   end
+
+  describe '#status_str' do
+    it 'should return a status string' do
+      status = SyncStatus.new 'name', :up_to_date
+      expect(status.status_str).to eq('name: up to date')
+    end
+
+    context 'when status_msg not nil' do
+      it 'should display status_msg' do
+        status = SyncStatus.new 'name', :error, 'error message'
+        expect(status.status_str).to eq('name: error: error message')
+      end
+    end
+  end
 end
 
 RSpec.describe GroupStatus do
   it 'should be a simple struct' do
-    status = GroupStatus.new [], 'message'
-    expect(status.items).to eq([])
-    expect(status.status_msg).to eq('message')
+    group = GroupStatus.new 'name', [], 'message'
+    expect(group.items).to eq([])
+    expect(group.status_msg).to eq('message')
   end
 
   describe '#kind' do
     it 'should include subitem status if all are equal' do
       status1 = SyncStatus.new 'name1', :up_to_date
       status2 = SyncStatus.new 'name2', :up_to_date
-      status = GroupStatus.new [status1, status2], 'message'
-      expect(status.items).to eq([status1, status2])
-      expect(status.kind).to eq(:up_to_date)
+      group = GroupStatus.new 'name', [status1, status2], 'message'
+      expect(group.items).to eq([status1, status2])
+      expect(group.kind).to eq(:up_to_date)
     end
 
     it 'should return nil if not all subitem status are not equal' do
       status1 = SyncStatus.new 'name1', :up_to_date
       status2 = SyncStatus.new 'name2', :error
-      status = GroupStatus.new [status1, status2], 'message'
-      expect(status.items).to eq([status1, status2])
-      expect(status.kind).to eq(nil)
+      group = GroupStatus.new 'name', [status1, status2], 'message'
+      expect(group.items).to eq([status1, status2])
+      expect(group.kind).to eq(nil)
     end
   end
-end
 
-RSpec.describe Status do
   describe '#status_str' do
+    it 'should print item if no subitems' do
+      group = GroupStatus.new 'group', []
+      expect(group.status_str).to eq('group:')
+    end
+
+    it 'should print itself and subitems' do
+      pending 'Not implemented yet'
+      status1 = SyncStatus.new 'name1', :up_to_date
+      status2 = SyncStatus.new 'name2', :error, 'error message'
+      group = GroupStatus.new 'group', [status1, status2]
+      expect(group.status_str).to eq(
+'group:
+    name1: up to date
+    name2: error: error message')
+    end
+
+    it 'should handle subgroups' do
+      pending 'Not implemented yet'
+      status1 = SyncStatus.new 'name1', :up_to_date
+      status2 = SyncStatus.new 'name2', :error, 'error message'
+      status3 = SyncStatus.new 'name3', :sync
+      group1 = GroupStatus.new 'group1', [status1, status2]
+      group = GroupStatus.new 'group', [group1, status3]
+      expect(group.status_str).to eq(
+'group:
+    group1:
+        name1: up to date
+        name2: error: error message
+    name3: sync')
+    end
   end
 end
 
