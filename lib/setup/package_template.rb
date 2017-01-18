53883565922276dbfc66ccfd35d9f91bba111413
Artur Spychaj <drognanar@gmail.com>
2017-01-17 21:54:14 -0800

    Rename backup to backups

--- lib/setup/package_template.rb
+++ lib/setup/package_template.rb
@@ -4,7 +4,7 @@ module Setup
 module Templates
 
 APPLICATIONS_TEMPLATE = File.read(File.join(__dir__, 'templates/applications.erb')).untaint
-BACKUP_TEMPLATE = File.read(File.join(__dir__, 'templates/backup.erb')).untaint
+BACKUPS_TEMPLATE = File.read(File.join(__dir__, 'templates/backups.erb')).untaint
 PACKAGE_TEMPLATE = File.read(File.join(__dir__, 'templates/package.erb')).untaint
 SYNC_TEMPLATE = File.read(File.join(__dir__, 'templates/sync.erb')).untaint
 
@@ -14,8 +14,8 @@ def applications(applications)
   ERB.new(APPLICATIONS_TEMPLATE, 2, '>').result(binding)
 end
 
-def backup
-  ERB.new(BACKUP_TEMPLATE, 2, '>').result(binding)
+def backups
+  ERB.new(BACKUPS_TEMPLATE, 2, '>').result(binding)
 end
 
 def package(package_name, files)
