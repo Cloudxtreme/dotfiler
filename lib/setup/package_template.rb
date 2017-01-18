f845ee5fa9d3ab9ef106f975941a3ba62716b7df
Artur Spychaj <drognanar@gmail.com>
2017-01-17 20:30:48 -0800

    Start implementing backup templates

--- lib/setup/package_template.rb
+++ lib/setup/package_template.rb
@@ -3,17 +3,27 @@ require 'erb'
 module Setup
 module Templates
 
-PACKAGE_TEMPLATE = File.read(File.join(__dir__, 'templates/package.erb')).untaint
 APPLICATIONS_TEMPLATE = File.read(File.join(__dir__, 'templates/applications.erb')).untaint
+BACKUP_TEMPLATE = File.read(File.join(__dir__, 'templates/backup.erb')).untaint
+PACKAGE_TEMPLATE = File.read(File.join(__dir__, 'templates/package.erb')).untaint
+SYNC_TEMPLATE = File.read(File.join(__dir__, 'templates/sync.erb')).untaint
 
 module_function
+
+def applications(applications)
+  ERB.new(APPLICATIONS_TEMPLATE, 2, '>').result(binding)
+end
+
+def backup
+  ERB.new(BACKUP_TEMPLATE, 2, '>').result(binding)
+end
 
 def package(package_name, files)
   ERB.new(PACKAGE_TEMPLATE, 2, '>').result(binding)
 end
 
-def applications(applications)
-  ERB.new(APPLICATIONS_TEMPLATE, 2, '>').result(binding)
+def sync
+  ERB.new(SYNC_TEMPLATE, 2, '>').result(binding)
 end
 
 end
