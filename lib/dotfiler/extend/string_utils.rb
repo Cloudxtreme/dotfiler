003bd75da7b8687e88eed559681b1193c8c88641
Artur Spychaj <drognanar@gmail.com>
2017-03-12 21:50:36 -0700

    Add a note for using the activesupport code

--- lib/dotfiler/extend/string_utils.rb
+++ lib/dotfiler/extend/string_utils.rb
@@ -1,6 +1,7 @@
-# TODO(drognanar): Get rid of the activesupport strip method.
-
 class String # rubocop:disable Documentation
+  # Copyright (c) 2005-2017 David Heinemeier Hansson
+  # Using MIT LICENSE
+  # Based on rails activesupport method.
   # @see https://github.com/rails/rails/blob/master/activesupport/lib/active_support/core_ext/string/strip.rb
   def strip_heredoc
     gsub(/^#{scan(/^[ \t]*(?=\S)/).min}/, ''.freeze)
